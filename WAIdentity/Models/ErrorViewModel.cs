1cc9ab4c64884690485a702d5752353bd0e822d9
Fall31 <lazomorenowilsonlazo@gmail.com>
2025-10-18 08:58:27 -0400

    Proyecto actualizado

--- WAIdentity/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WAIdentity.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
